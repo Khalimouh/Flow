8950473adba6f9d8edfd921bb43f00f0e5e36771
Akli Hamitouche <akli.m.hamitouche@gmail.com>
2020-04-02 19:46:39 +0200

    Version presque finale

--- src/main.c
+++ src/main.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <assert.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct {
 	char L[3][17];  //Registres
 	char F[9];	    //Fonction
 }LFSR;
 
-void DebugLFSR(LFSR* Geffe){
-	for (int i = 0; i < 3; ++i)	{
-		printf("%s\n", Geffe->L[i]);
-	}
-  	printf("%s\n", Geffe->F);
+void checkArgs ( int argc)	{
+	if(argc != 3)	{ 
+		fprintf(stderr, "Flow: opérande manquant\nUsage: Usage  : ./Flow fichier n\n");
+		exit(1);
+	}	
 }
 
-int FonctionF(char x0, char x1, char x2){
+void timer (void)	{
+	for (int i = 0; i < 3; i++)	{
+		fflush (stdout);
+		printf (".");
+		fflush ( stdout);
+		sleep ( 1);
+	}
+    printf ("\n");
+}
+
+void printSequence ( int * s, int n)	{
+	printf ("Affichage des n premiers bit de la sequence chiffrante :\n\t");
+	for (int i = 0; i < n; i++)
+		printf ("%d ", s[i]);
+	printf ("\n");	
+}
+
+int filteringF(char x0, char x1, char x2){
 	return (x0*1) + (x1*2) + (x2*4);
 }
 
-void decale ( LFSR* Geffe)	{
+int shift ( LFSR * Geffe)	{
+	int indice = filteringF ( (Geffe->L[0][0] - '0'), (Geffe->L[1][0] - '0'), (Geffe->L[2][0] - '0'));
 	short tmp0 = (Geffe->L[0][0] - '0') ^ (Geffe->L[0][1] - '0') ^ (Geffe->L[0][4] - '0') ^ (Geffe->L[0][7] - '0'),
 		  tmp1 = (Geffe->L[1][0] - '0') ^ (Geffe->L[1][1] - '0') ^ (Geffe->L[1][7] - '0') ^ (Geffe->L[1][11] - '0'),
 		  tmp2 = (Geffe->L[2][0] - '0') ^ (Geffe->L[2][2] - '0') ^ (Geffe->L[2][3] - '0') ^ (Geffe->L[2][5] - '0');
@@ -31,17 +49,22 @@ void decale ( LFSR* Geffe)	{
 	Geffe->L[0][0] = tmp0 + '0';
 	Geffe->L[1][0] = tmp1 + '0';
 	Geffe->L[2][0] = tmp2 + '0';
+	return Geffe->F[indice] - '0';
 }
 
 
-void Decalage(LFSR* Geffe, char* bitL0, char* bitL1, char* bitL2){
-	//Sortie du bit de poids faible
-	*bitL0 = Geffe->L[0][0]; 
-	*bitL1 = Geffe->L[1][0];
-	*bitL2 = Geffe->L[2][0];
-	//Décalage
-	//decale ( Geffe);
-
+int * generate(LFSR* Geffe, int n) { //char* bitL0, char* bitL1, char* bitL2){
+	fprintf ( stdout, "Generation de la suite chiffrante ");
+	int * s = (int *) malloc ( n * sizeof (int));
+	if ( !s)	{
+		fprintf ( stderr, "Erreur lors de l'allocation de la clé S\n");
+		exit(1);
+	}
+	for (int i = 0; i < n; i++)	{
+		s[i] = shift ( Geffe);
+	}
+	timer ();
+	return s;
 }
 
 
@@ -55,21 +78,25 @@ void initialiserLFSR(const char* path,LFSR* gen){
 	ssize_t read;
 	size_t len = 1;
 	int i = 0;
-   	printf("Initialisation du LFSR\n");
+   	printf("Initialisation du LFSR ");
     while ((read = getline(&buffer, &len, file)) != -1) {
-       read = getline(&buffer, &len, file);
-       strcpy(gen->L[i],buffer);
-       gen->L[i][16] = 0;
-       //gerer derniere ligne
+		read = getline(&buffer, &len, file);
+		// Gerer les valeurs k0, k1, k2
+		if ( i < 3)	{  
+		   strcpy(gen->L[i],buffer);
+		   gen->L[i][16] = 0;
+	   }
+       //gerer derniere ligne (L)
        if(i == 3)	{
 		   strncpy(gen->F,buffer,8);
 		   gen->F[8] = 0;
 	   }
        i++;
     }
-
+    timer ();
 	free(buffer);
 	fclose(file);
+	
 }
 
 /**
@@ -78,22 +105,16 @@ void initialiserLFSR(const char* path,LFSR* gen){
 
 int main(int argc, char const *argv[])
 {
-	if(argc != 3)	{ 
-		fprintf(stderr, "Usage: Usage  : ./Flow fichier n\n");
-		exit(0);
-	}
+	checkArgs ( argc);
 	//Ouvir le fichier et initialiser le LFSR
 	
 	LFSR* Geffe = (LFSR*) malloc(sizeof(LFSR));
 	initialiserLFSR(argv[1],Geffe);
 	
-	DebugLFSR(Geffe);
-	decale(Geffe);
-	
-	printf ("\n\nNouveau test\n");
-	DebugLFSR(Geffe);
-	//printf("%d\n", FonctionF(1, 1, 1));
+	int * s = generate ( Geffe, atoi ( argv[2]));
+	printSequence ( s, atoi ( argv[2]));
   	
   	free(Geffe);
+  	free ( s);
 	return 0;
 }
